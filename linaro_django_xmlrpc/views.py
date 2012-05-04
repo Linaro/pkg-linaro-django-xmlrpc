f9ec42137559926c32db7fc60fd21ad78a57f5a2
Michael Hudson-Doyle <michael.hudson@linaro.org>
2012-05-04 14:50:42 +1200

    update import to reflect location of csrf_exempt decorator in Django 1.4

--- linaro_django_xmlrpc/views.py
+++ linaro_django_xmlrpc/views.py
@@ -24,12 +24,12 @@ import base64
 import logging
 
 from django.contrib.auth.decorators import login_required
-from django.contrib.csrf.middleware import csrf_exempt
 from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
 
 from linaro_django_xmlrpc.models import (
     AuthToken,
