e3ea314e51fd28ba705517aaadd5fe2b1275301e
Neil Williams <codehelp@debian.org>
2013-11-22 11:04:51 +0000

    Convert to an upstream packaging branch.

--- linaro_django_xmlrpc/test_project/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls.defaults import (
-    patterns, url, include, handler500, handler404)
-
-urlpatterns = patterns(
-    '',
-    url(r'', include('linaro_django_xmlrpc.urls')),
-    url(r'^accounts/', include('django.contrib.auth.urls')),
-)
